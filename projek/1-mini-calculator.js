02c703ee2439c0d68576e7d455aee64ddfd423d5
SakhaXPPLG2 <sakhaall333@gmail.com>
2025-10-05 11:13:29 +0700

    SELESAI PROJEK CALCULATOR PAK

---
+++ projek/1-mini-calculator.js
@@ -0,0 +1,81 @@
+// Pertambahan Function
+
+const Pertambahan = () => {
+    let a = 69;
+    let b = 5;
+
+    console.log("Function Pertambahan:");
+    console.log("-------------");
+    console.log(`Penjumlahan ${a} + ${b} = ${a + b}`);
+}
+
+Pertambahan();
+
+// Pengurangan Function
+
+const Pengurangan = () => {
+    const c = 44;
+    const d = 20;
+
+    console.log("-------------");
+    console.log("Function Pengurangan:");
+
+    console.log("-------------");
+    console.log(`Penjumlahan ${c} - ${d} = ${c - d}`);
+}
+
+Pengurangan();
+
+// Perkalian Function
+
+const Perkalian = (e = 2, f = 99) => {
+    console.log("-------------");
+    console.log("Function Perkalian:");
+    
+    console.log("-------------");
+    console.log(`Penjumlahan ${e} x ${f} = ${e * f}`)
+}
+
+Perkalian();
+
+// Pembagian Function 
+
+const Pembagian = () => {
+    let g = 4;
+    let h = 2;
+
+    console.log("-------------");
+    console.log("Function Pembagian:");
+    
+    console.log("-------------");
+    console.log(`Penjumlahan ${g} : ${h} = ${g / h}`);
+}
+
+Pembagian();
+
+// Perpangkatan Function
+
+const Perpangkatan = (i = 11, j = 5) => {
+    console.log("-------------");
+    console.log("Function Berpangkatan:");
+    
+    console.log("-------------");
+    console.log(`Penjumlahan ${i} berpangkat ${j} = ${i ** j}`);
+}
+
+Perpangkatan();
+
+// Sisa bagi / Modulus Function
+
+const SisaBagi = () => {
+    let k = 11;
+    let l = 5;
+
+    console.log("-------------");
+    console.log("Function Sisa Bagi / Modulus:");
+    
+    console.log("-------------");
+    console.log(`Penjumlahan ${k} Sisa Bagi ${l} = ${k % l}`);
+}
+
+SisaBagi();
